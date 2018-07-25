a5ff89f8073ee70a1080ca216873ba55b9fb0855
Aronja <anovicky@thoughtworks.com>
2018-07-25 10:19:08 +0100

    Chapter 3 make equalityTest pass

--- test/testEquality.java
+++ test/testEquality.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class testEquality {
@@ -7,5 +8,6 @@ public class testEquality {
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
